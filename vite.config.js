ee9a036b4b16804225f0add045be24e9d58e32d4
Tom Taco <28072822+wgregoryw@users.noreply.github.com>
2025-06-19 14:21:51 -0700

    Configure for custom domain hantyumi.cloud

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/speedreader/', // Replace with your repository name
+  base: '/', // Using custom domain, so base is root
   plugins: [react()],
 });
